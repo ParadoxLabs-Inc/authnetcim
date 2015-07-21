64c488b956e4cf7e852b20309d37c1ca98be4563
Ryan Hoerr <ryan@paradoxlabs.com>
2015-07-21 15:07:54 -0400

    Magento 2 module skeleton and config

---
+++ Model/Method.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Authorize.Net CIM payment method object
+ *
+ * Paradox Labs, Inc.
+ * http://www.paradoxlabs.com
+ * 717-431-3330
+ *
+ * Need help? Open a ticket in our support system:
+ *  http://support.paradoxlabs.com
+ *
+ * @author        Ryan Hoerr <magento@paradoxlabs.com>
+ * @license       http://store.paradoxlabs.com/license.html
+ */
+
+namespace ParadoxLabs\Authnetcim\Model;
+
+// TODO: this
+class Method extends \ParadoxLabs\TokenBase\Model\AbstractMethod
+{
+
+}
