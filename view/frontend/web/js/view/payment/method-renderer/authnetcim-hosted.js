b18ddb8b90bef4b2c666f169e2045f94d4d1573a
Ryan <ryan@paradoxlabs.com>
2023-05-10 14:44:36 -0400

    Changed license to Apache 2.0.

--- view/frontend/web/js/view/payment/method-renderer/authnetcim-hosted.js
+++ view/frontend/web/js/view/payment/method-renderer/authnetcim-hosted.js
@@ -1,13 +1,20 @@
 /**
- * Paradox Labs, Inc.
- * http://www.paradoxlabs.com
- * 717-431-3330
+ * Copyright © 2015-present ParadoxLabs, Inc.
  *
- * Need help? Open a ticket in our support system:
- *  http://support.paradoxlabs.com
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * @author      Ryan Hoerr <support@paradoxlabs.com>
- * @license     http://store.paradoxlabs.com/license.html
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Need help? Try our knowledgebase and support system:
+ * @link https://support.paradoxlabs.com
  */
 
 define(
